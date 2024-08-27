500fcd0e44e90990355103781e8f56f4694d0a58
chr1swill <alexanderwilliams20003@gmail.com>
2024-08-27 16:39:23 -0400

    feat: debounce write message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,57 @@
 import { opfsWorkerMessage } from "./globals.js";
 
+class DebounceWriteMessage {
+    /**@type{Readonly<number>}*/
+    #debounceTime = 5000;
+
+    /**@type{number}*/
+    #timeout = -1;
+
+    /**
+     * @param{ Worker } worker 
+     * @param{ HTMLTextAreaElement | null } textInput
+     */
+    constructor(worker, textInput) {
+        this.textInput = textInput;
+        if (this.textInput === null) {
+            console.error("Could not find element with id: #note_text_input");
+            return;
+        };
+
+        if (!(worker instanceof Worker)) {
+            console.error("Invalid worker was provided note able to post write message", worker);
+            return;
+        };
+        this.worker = worker;
+    }
+
+    run() { 
+        const that = this;
+
+        if (that.#timeout !== -1) {
+            clearTimeout(that.#timeout);
+        };
+
+        that.#timeout = setTimeout(function() {
+            clearTimeout(that.#timeout);
+
+            if (that.textInput === null) {
+                console.error("Could not find element with id: #note_text_input");
+                return;
+            };
+
+            const message = { type: opfsWorkerMessage.WRITE, content: that.textInput.value };
+            console.debug("message being sent to worker: ", message);
+            if (!(that.worker instanceof Worker)) {
+                console.error("Invalid worker was provided note able to post write message", that.worker);
+                return;
+            };
+
+            that.worker.postMessage(message);
+        }, that.#debounceTime);
+    };
+};
+
 function main() {
     const noteTextInput = /**@type{ HTMLTextAreaElement | null }*/
         (document.getElementById("note_text_input"));
@@ -14,7 +66,9 @@ function main() {
         return;
     };
 
+
     const opfsWorker = new Worker("./js/opfs-worker.js", { type: 'module' });
+    const writeMessageDebouncer = new DebounceWriteMessage(opfsWorker, noteTextInput);
 
     opfsWorker.onmessage = function(e) {
         const message = e.data;
@@ -31,9 +85,7 @@ function main() {
     };
 
     noteTextInput.oninput = function() {
-        const text = noteTextInput.value;
-        console.debug("message being sent to worker: ", { type: opfsWorkerMessage.WRITE, content: text });
-        opfsWorker.postMessage({ type: opfsWorkerMessage.WRITE, content: text });
+        writeMessageDebouncer.run();
     };
 };
 main();
