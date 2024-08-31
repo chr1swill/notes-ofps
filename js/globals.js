fbe246845e20cae5ce5f45056489b7371552ea28
chr1swill <alexanderwilliams20003@gmail.com>
2024-08-31 10:32:45 -0400

    feat: add function for coverting string to snake case

--- js/globals.js
+++ js/globals.js
@@ -4,3 +4,26 @@ export const opfsWorkerMessage = Object.freeze({
     DELETE: 2,
     MOVE: 3,
 });
+
+/**
+ *
+ * for the one use case it is faster than a regex :)
+ *
+ * @param{string} input
+ * @returns {string}
+ */
+export function toSnakeCase(input) {
+    const inputLength = input.length;
+    const output = new Array(inputLength);
+    for (let i = 0; i < inputLength; i++) {
+        //@ts-ignore
+        if (input.i !== ' ') { 
+            //@ts-ignore
+            output.i = input.i;
+        } else {
+            //@ts-ignore
+            output.i = "_";
+        };
+    };
+    return output.join('');
+};
